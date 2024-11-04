4bbd980466e3f623b4784f31e0f779c84150cc4b
Jayesh <jayeshdpadiya1802@gmail.com>
2024-11-04 19:54:27 +0530

    login

---
+++ back-end/server/contexts/AuthContext/index.jsx
@@ -0,0 +1,26 @@
+const AuthContext= React.createContext();
+export function AuthProvider({children}){
+    const[currentUser,setCurrentUser] =useState(null);
+    const[userLoggedIn,setUserLoggedIn]=useState(false);
+    const[loading, setLoading]=useState(true);
+
+    useEffect(() => {
+     const unsubscribe=onAuthStateChanged(auth,initializeUser);
+     return unsubscribe;
+    }, [])
+    async function initializeUser(user){
+        if(user){
+            setCurrentUser({...user});
+            setUserLoggedIn(true);
+        }else{
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+    const value={
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+}
